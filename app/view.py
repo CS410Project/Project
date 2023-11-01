1a014fb46dad599e7530ee1e0c1577f8e27471cd
KentonJack <kenton.jack.robinson@hotmail.com>
2023-10-31 20:03:26 -0400

    add simple in-memory caching

--- app/view.py
+++ app/view.py
@@ -1,12 +1,41 @@
+from collections import defaultdict
 from flask import Flask, jsonify, request
+from datetime import datetime
+from typing import Dict
 
 app = Flask(__name__)
 
+# simple cache for video play counts
+# {
+#   "video_url_1": 1,
+#   "video_url_2": 2,
+#   ....
+# }
+video_cache: Dict[str, int] = defaultdict(int)
+
+# partitioned video_cache for each time lapse
+# {
+#   "2020-01-01 00:00:00": {
+#     "video_url_1": 1,
+#     "video_url_2": 2,
+#     ....
+#   },
+#   "2020-01-01 01:00:00": {
+#     "video_url_1": 1,
+#     "video_url_2": 2,
+#     ....
+#   },
+#  ....
+# }
+video_cache_partitions: Dict[str, Dict[str, int]] = defaultdict(lambda: defaultdict(int))
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root_url():
+    return 'Simple Video Platform Root'
 
 
+#=================================================Sample Showcases==========================================================
 # Define a route for handling GET requests
 # Usage: Open in browser http://localhost:5000/data
 @app.route('/data', methods=['GET'])
@@ -45,7 +74,33 @@ def post_data():
         return jsonify(response_data), 201  # Respond with 201 Created status
     else:
         return jsonify({'error': 'Invalid JSON data'}), 400  # Respond with 400 Bad Request status
+#=======================================================================================================================
+
+
+
+# A simple GET route simulating user hit video play button
+@app.route('/play/<int:video_id>', methods=['GET'])
+def play_video(video_id):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:00:00')
+    video_cache_partitions[timestamp][str(video_id)] += 1
+
+
+# TODO: define a simple GET route to return video play count for a given time lapse
+# Usage: curl http://localhost:5000/play_count?start_time=2020-01-01%2000:00:00&end_time=2020-01-01%2012:00:00
+@app.route('/play_count', methods=['GET'])
+def get_video_play_count():
+    start_time = request.args.get('start_time')
+    end_time = request.args.get('end_time')
+    if not start_time or not end_time:
+        return jsonify({'error': 'Invalid start_time or end_time'}), 400
+    raise NotImplementedError("TODO: implement this route")
+
+
+# clear global cache resetting video play count
+def initialize_video_count_cache():
+    video_cache_partitions.clear()
 
 
 if __name__ == '__main__':
+    initialize_video_count_cache()
     app.run()
