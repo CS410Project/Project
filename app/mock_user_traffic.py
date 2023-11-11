9fe4665f1d19c474c2a3e393d2eddb1bdd4731ed
TyZhou <77256005+ty-Z@users.noreply.github.com>
2023-11-11 22:40:12 +0800

    Update mock_user_traffic.py

--- app/mock_user_traffic.py
+++ app/mock_user_traffic.py
@@ -1,11 +1,19 @@
+import threading
 import random
-# List of video URLs
-video_list = [
-   "https://www.youtube.com/watch?v=video1",
-   "https://www.youtube.com/watch?v=video2",
-   "https://www.youtube.com/watch?v=video3",
-   "https://www.youtube.com/watch?v=video4",
-   "https://www.youtube.com/watch?v=video5",
-]
-# Simulate a user opening a random video
-random_video = random.choice(video_list)
+import time
+# List of 100 videos
+videos = list(range(1, 101))
+def open_random_video(user_id):
+   video_id = random.choice(videos)
+   # Simulate some processing time for watching the video
+   time.sleep(random.uniform(1, 5))
+# Number of users
+num_users = 10000
+# Create threads for each user
+threads = [threading.Thread(target=open_random_video, args=(i,)) for i in range(num_users)]
+# Start the threads
+for thread in threads:
+   thread.start()
+# Wait for all threads to finish
+for thread in threads:
+   thread.join()
